c0e79b5ab912d3b6ff5d293cabbdf13ddeb2b33d
Keefer Taylor <keefer.t@gmail.com>
2020-05-12 03:04:52 +0900

    rename errors

--- src/PayID/pay-id-error.ts
+++ src/PayID/pay-id-error.ts
@@ -1,5 +1,11 @@
+// Disable multiple classes to accommodate the switch to idiomatic style naming.
+// TODO(keefertaylor): Remove this when migration is complete.
+/* eslint-disable  max-classes-per-file */
+
 /**
  * Types of errors that originate from PayID.
+ *
+ * @deprecated Please use the idiomatically named `PayIdErrorType` class instead.
  */
 export enum PayIDErrorType {
   InvalidPayID,
@@ -11,6 +17,8 @@ export enum PayIDErrorType {
 
 /**
  * Represents errors thrown by PayID components of the Xpring SDK.
+ *
+ * @deprecated Please use the idiomatically named `PayIdError` class instead.
  */
 export default class PayIDError extends Error {
   /**
@@ -32,6 +40,46 @@ export default class PayIDError extends Error {
    */
   public constructor(
     public readonly errorType: PayIDErrorType,
+    message: string | undefined = undefined,
+  ) {
+    super(message)
+  }
+}
+
+/**
+ * Types of errors that originate from PayID.
+ */
+export enum PayIdErrorType {
+  InvalidPayId,
+  MappingNotFound,
+  UnexpectedResponse,
+  Unimplemented,
+  Unknown,
+}
+
+/**
+ * Represents errors thrown by PayID components of the Xpring SDK.
+ */
+export class PayIdError extends Error {
+  /**
+   * Default errors.
+   */
+  public static unimplemented = new PayIdError(
+    PayIdErrorType.Unimplemented,
+    'Unimplemented',
+  )
+
+  public static invalidPayId = new PayIdError(
+    PayIdErrorType.InvalidPayId,
+    'Invalid payment pointer',
+  )
+
+  /**
+   * @param errorType The type of error.
+   * @param message The error message.
+   */
+  public constructor(
+    public readonly errorType: PayIdErrorType,
     message: string | undefined = undefined,
   ) {
     super(message)
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,12 @@ export {
 } from 'xpring-common-js'
 export { PaymentRequest, PaymentResult, AccountBalance, IlpClient } from './ILP'
 export { default as PayIDClient } from './PayID/pay-id-client'
-export { PayIDErrorType, default as PayIDError } from './PayID/pay-id-error'
+export {
+  PayIdError,
+  PayIdErrorType,
+  PayIDErrorType,
+  default as PayIDError,
+} from './PayID/pay-id-error'
 export { default as XRPLNetwork } from './Common/xrpl-network'
 export { default as ComplianceType } from './PayID/compliance-type'
 export { default as XpringClient } from './Xpring/xpring-client'
